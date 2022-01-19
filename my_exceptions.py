85432ad288ec0dc7509b233e93c3b1d9fcc19959
Ann Sokolova <fullsokan@gmail.com>
2022-01-19 16:43:36 +0300

    exceptions (continuation)

--- my_exceptions.py
+++ my_exceptions.py
@@ -3,42 +3,131 @@
 """
 
 
-# try-except
+# try/except
 def func_with_exception_zero_div(a, b):
+    # an exception may appear inside this block
     try:
         result = a / b
         return result
+    # the code that should be executed when an error occurs
     except Exception:  # except ZerodivisionError: - best way; except Exception: - too broad exception clauses
         print('\tZerodivisionError!!!')
         return "ZerodivisionError"
 
 
-# try-except/else
-def func_with_exception(a):
+# try/except/else
+def func_with_exception_ind_err(a):
     try:
         mas = [1, 2, 3, 4, 5]
         result = mas[a]
     except IndexError:
         print('\tIndexError!!!')
         return "IndexError"
+    # the code is executed if there were no errors
     else:
         print('\tno errors')
         return result
 
 
-# try-except/finally
-# try-except/else/finally
+# try/finally
+def func_exc_fin(file_name):
+    file = open(file_name, 'r')
+    print('file was opened...')
+    try:
+        lines = file.readlines()
+        print(lines[0:])
+    # the code that is executed at the conclusion of the work (it does not matter if there was an error or not)
+    # it is usually used to close files, for example (for files, it is better to use with/as construction)
+    finally:
+        file.close()
+        print('file was closed...')
+
+
+# try/except/finally
+def func_with_exception_fin(a):
+    try:
+        mas = [1, 2, 3, 4, 5]
+        return mas[a]
+    except IndexError:
+        print('\tIndexError!!!')
+        return "IndexError"
+    finally:
+        print('\tfinally')
+
+
+# try/except/else/finally
+def func_with_exception_ef(a):
+    try:
+        mas = [1, 2, 3, 4, 5]
+        res = mas[a]
+    except IndexError as err:  # as saves the error as a variable for further access to it
+        print('\t', err)
+    else:
+        print('\telse')
+    finally:
+        print('\tfinally')
+
+
+# try/except with several except-blocks
+def excs_division():
+    a = input('a = ')
+    b = input('b = ')
+    try:
+       res = float(a) / float(b)
+    except ZeroDivisionError:
+        print('ZeroDivisionError')
+    except ValueError:
+        print('ValueError')
+    else:
+        print(res)
+
+
+# try/except with several except in one
+def exc_division():
+    a = input('a = ')
+    b = input('b = ')
+    try:
+       res = float(a) / float(b)
+    except (ZeroDivisionError, ValueError) as err:
+        print(err)
+    else:
+        print(res)
 
 
 if __name__ == "__main__":
-    print('construction try-except')
-    res1 = func_with_exception_zero_div(10, 5)
-    res2 = func_with_exception_zero_div(10, 0)
-    print(f'results of functions:\n\tfunction 1 = {res1}\n\tfunction 2 = {res2}\n')
+    print('construction try/except:')
+    print('without error:')
+    func_with_exception_zero_div(10, 5)
+    print('with error:')
+    func_with_exception_zero_div(10, 0)
 
-    print('construction try-except/else')
-    res1 = func_with_exception(2)
-    res2 = func_with_exception(10)
-    print(f'results of functions:\n\tfunction 1 = {res1}\n\tfunction 2 = {res2}')
+    print('\nconstruction try/except/else:')
+    print('without error:')
+    func_with_exception_ind_err(2)
+    print('with error:')
+    func_with_exception_ind_err(10)
 
-    print('')
+    print('\nconstruction try/finally:')
+    print('results of function (without error):')
+    func_exc_fin("file.txt")
+
+    print('\nconstruction try/except/finally:')
+    print('without error:')
+    func_with_exception_fin(2)
+    print('with error:')
+    func_with_exception_fin(10)
+
+    print('\nconstruction try/except/else/finally:')
+    print('without error:')
+    func_with_exception_ef(2)
+    print('with error:')
+    func_with_exception_ef(10)
+
+    print('\nconstruction try/except with several except-blocks:')
+    excs_division()
+
+    print('\nconstruction try/except with several except in one:')
+    exc_division()
+
+    print('\noperator raise:')
+    # raise
