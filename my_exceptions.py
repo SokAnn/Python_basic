59b126685589d23be8e6c44b2ffdbba4afbb45b8
Ann Sokolova <fullsokan@gmail.com>
2022-02-08 17:26:41 +0300

    exceptions (raise & assert)

--- my_exceptions.py
+++ my_exceptions.py
@@ -32,15 +32,15 @@ def func_with_exception_ind_err(a):
 # try/finally
 def func_exc_fin(file_name):
     file = open(file_name, 'r')
-    print('file was opened...')
+    print('\tfile was opened...')
     try:
         lines = file.readlines()
-        print(lines[0:])
+        print('\t', lines[0:])
     # the code that is executed at the conclusion of the work (it does not matter if there was an error or not)
     # it is usually used to close files, for example (for files, it is better to use with/as construction)
     finally:
         file.close()
-        print('file was closed...')
+        print('\tfile was closed...')
 
 
 # try/except/finally
@@ -70,8 +70,8 @@ def func_with_exception_ef(a):
 
 # try/except with several except-blocks
 def excs_division():
-    a = input('a = ')
-    b = input('b = ')
+    a = input('\ta = ')
+    b = input('\tb = ')
     try:
        res = float(a) / float(b)
     except ZeroDivisionError:
@@ -84,14 +84,38 @@ def excs_division():
 
 # try/except with several except in one
 def exc_division():
-    a = input('a = ')
-    b = input('b = ')
+    a = input('\ta = ')
+    b = input('\tb = ')
     try:
        res = float(a) / float(b)
     except (ZeroDivisionError, ValueError) as err:
         print(err)
     else:
         print(res)
+
+
+# raise
+def func_raise(var_max):
+    try:
+        if var_max < 500:
+            # forcing an exception call
+            raise Exception('max must be more than 500')
+    except Exception as exc:
+        print('\tmistake!!!')
+        print('\tException: ', exc)
+    else:
+        print('\tno errors...')
+
+
+# assert
+def func_assert(var):
+    try:
+        # syntax: condition, message (optional) under false condition
+        assert var != 0, 'variable must be True value'
+    except AssertionError:
+        print('\tAssertionError')
+    else:
+        print('\tno errors...')
 
 
 if __name__ == "__main__":
@@ -130,4 +154,13 @@ if __name__ == "__main__":
     exc_division()
 
     print('\noperator raise:')
-    # raise
+    print('without error:')
+    func_raise(1000)
+    print('with error:')
+    func_raise(100)
+
+    print('\noperator assert:')
+    print('without error:')
+    func_assert(1)
+    print('with error:')
+    func_assert(0)
